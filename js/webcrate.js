eb9a75e239b723c370c5071c0b9c0c8e432fc12a
cyrusliew02 <cyrusliew02@gmail.com>
2018-04-07 23:39:46 +1200

    Fullpage

--- js/webcrate.js
+++ js/webcrate.js
@@ -48,21 +48,40 @@ $(document).ready(function () {
         },
         "retina_detect": true
     });
-    var count_particles, stats, update;
-    stats = new Stats;
-    stats.setMode(0);
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.left = '0px';
-    stats.domElement.style.top = '0px';
-    document.body.appendChild(stats.domElement);
-    count_particles = document.querySelector('.js-count-particles');
-    update = function () {
-        stats.begin();
-        stats.end();
-        if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-            count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+
+    //fullpage
+    const sectionAnchors = [];
+
+    $('.section').each(function(){
+        sectionAnchors.push($(this).attr('data-id'));
+    });
+
+    console.log(sectionAnchors);
+
+    $('#main-container').fullpage({
+        anchors: sectionAnchors,
+        onLeave: function(index, nextIndex, direction){
+            const aniSpeed = 300;
+            const introItems = $('header .inner-wrapper > *:not(.logo)');
+            const logo = $('header .inner-wrapper > .logo');
+            const header = $('header');
+
+            //after leaving section 2
+            if(index == 1 && direction =='down'){
+                introItems.fadeOut();
+                header.addClass('sticky');
+            }
+
+            else if(index == 2 && direction == 'up'){
+                introItems.fadeIn();
+                header.removeClass('sticky');
+            }
+
+            if(nextIndex > 1) {
+                const sectionHeader = $('.section').eq(nextIndex).find('.header');
+                const sectionInfo = header.find('.section-info');
+
+            }
         }
-        requestAnimationFrame(update);
-    };
-    requestAnimationFrame(update);
+    });
 });
